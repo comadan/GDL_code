ec4534afa7b9fe6cd0f4c4ced4daa63d00791ab3
Daniel Yoo <comadan@gmail.com>
2020-06-10 14:13:44 -0700

    add stub for cycle gan.

---
+++ NotebooksFromScratch/models/CycleGenerativeAdversarialNetwork.py
@@ -0,0 +1,20 @@
+
+class CycleGenerativeAdversarialNetwork():
+    def __init__(self):
+        return
+
+    def _build_unet(self):
+        return
+
+
+    def _build_resent(self):
+        return
+    
+
+    def _build(self):
+        return
+
+
+    def compile(self):
+        return
+
